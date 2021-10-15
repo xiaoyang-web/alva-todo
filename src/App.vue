c20a6e4fe3f3a6b088725cdf587569a8cd4ec6bc
alva-yky <a356510493@hotmail.com>
2021-10-15 13:29:46 +0800

    修改项目全屏代码

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,7 @@ export default {
 
 <style>
 #app {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
